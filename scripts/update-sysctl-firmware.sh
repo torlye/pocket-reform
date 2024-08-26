afbc2a13d0de84971bfe999e0a33c9af15701581
Lukas F. Hartmann <lukas@mntre.com>
2024-08-26 14:16:44 +0200

    scripts: update sysctl firmware from local build

--- scripts/update-sysctl-firmware.sh
+++ scripts/update-sysctl-firmware.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-set -e
-
-FWURL=https://source.mnt.re/reform/pocket-reform/-/jobs/4892/artifacts/raw/pocket-reform-sysctl-fw/build/sysctl.uf2
-FWPATH=/tmp/sysctl.uf2
+FWPATH=../pocket-reform-sysctl-fw/build/sysctl.uf2
 # change to 2e8a:0003 if rp2040 is in "Raspberry Pi RP2 Boot" mode
 USBDEV=2e8a:000a
 
-# make sure we have picotool and wget
-apt install -y picotool wget
+# make sure the file is there
+strings $FWPATH | grep PREF1SYS > /dev/null
+if [[ $? != 0 ]]; then 
+	echo "Firmware file $FWPATH is invalid, exiting."
+	exit 1
+fi
 
-# identify systemcontroller on usb bus
+set -e
+
+# make sure we have picotool
+apt install -y picotool
+
+# identify system controller on usb bus
 COUNT=$(lsusb | grep $USBDEV | wc -l)
 if [[ $COUNT != 1 ]];
 then echo "RP2040 System Controller device not found or more than one found, exiting."
-     exit 1
+	exit 1
 fi
 
 # extract usb bus and device address numbers
@@ -26,16 +32,6 @@ ADDR=${ADDR//:}
 # make sure these are integers
 BUS=$(expr $BUS + 0)
 ADDR=$(expr $ADDR + 0)
-
-# download the firmware binary
-wget -O $FWPATH $FWURL
-
-# verify integrity
-HASH=$(md5sum $FWPATH | cut -d ' ' -f 1)
-if [[ "$HASH" != "e11a1be35444f7e72cc58e32858e8f86" ]]
-then echo "Downloaded $FWPATH failed integrity check, exiting."
-     exit 2
-fi
 
 # ask for confirmation
 echo "Ready to flash System Controller (bus $BUS, address $ADDR). Make sure all your work is saved--the system will power down after the process is completed."

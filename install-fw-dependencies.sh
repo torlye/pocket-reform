ab6a0f1c3a1ba513b17b582d0fe6e78243e665b2
Nano CodeBug <nanocodebug@gmail.com>
2024-09-03 12:13:22 -0700

    restore prev build scripts

--- install-fw-dependencies.sh
+++ install-fw-dependencies.sh
@@ -1,9 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 sudo apt install build-essential pkg-config libusb-1.0-0-dev cmake gcc-arm-none-eabi tio picotool
 
-mkdir lib
-cd lib
 git clone --depth 1 https://github.com/raspberrypi/pico-sdk
 cd pico-sdk
 git submodule init
@@ -11,4 +9,4 @@ git submodule update --depth 1
 cd ..
 git clone --depth 1 https://github.com/raspberrypi/pico-extras
 git clone --depth 1 https://github.com/hathach/tinyusb
-cd ..
+

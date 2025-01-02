f64db25f88ba9042f3a83c87c32b430732befb8b
Chris Hofstaedtler <chris@hofstaedtler.name>
2025-01-02 20:21:28 +0100

    pocket-hid: add fwupd and reset capability

---
+++ pocket-reform-keyboard-fw/pocket-hid/ds20.quirk.tpl
@@ -0,0 +1,8 @@
+[USB\VID_1209&PID_6D06]
+Vendor=MNT Research
+Plugin=rp_pico
+Flags=internal
+VersionFormat=number
+Version=@FWCAB_RELEASE_VERSION@
+Icon=keyboard
+CounterpartGuid=BLOCK\VEN_2E8A&DEV_0003
--- pocket-reform-shared-fw/inc/reset_interface.h
+++ pocket-reform-shared-fw/inc/reset_interface.h
@@ -6,7 +6,9 @@
 #pragma once
 
 // Enable/disable resetting into BOOTSEL mode if the host sets the baud rate to a magic value (PICO_STDIO_USB_RESET_MAGIC_BAUD_RATE), type=bool, default=1, group=pico_stdio_usb
+#ifndef RESET_INTERFACE_ENABLE_RESET_VIA_BAUD_RATE
 #define RESET_INTERFACE_ENABLE_RESET_VIA_BAUD_RATE 1
+#endif
 
 // baud rate that if selected causes a reset into BOOTSEL mode (if PICO_STDIO_USB_ENABLE_RESET_VIA_BAUD_RATE is set), default=1200, group=pico_stdio_usb
 #define RESET_INTERFACE_RESET_MAGIC_BAUD_RATE 1200

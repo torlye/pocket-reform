1b24864c656039e97ae37fcdea32b90732430909
Chris Hofstaedtler <chris@hofstaedtler.name>
2025-01-07 16:25:49 +0100

    fw: allow non-numeric firmware versions

--- pocket-reform-sysctl-fw/build.sh
+++ pocket-reform-sysctl-fw/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 export PICO_SDK_PATH=/usr/src/pico-sdk
-export MNTRE_FIRMWARE_VERSION=$(date +%Y%m%d)
+if [ -z "$MNTRE_FIRMWARE_VERSION" ]; then
+    export MNTRE_FIRMWARE_VERSION=$(date +%Y%m%d)
+fi
 
 mkdir -p build
 cd build

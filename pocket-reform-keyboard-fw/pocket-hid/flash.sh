b751be08991814a28dcf3ede0a50876253c4ed3d
Johannes Schauer Marin Rodrigues <josch@mister-muffin.de>
2025-05-22 13:41:52 +0200

    pocket-reform-keyboard-fw/pocket-hid/flash.sh: be interactive

--- pocket-reform-keyboard-fw/pocket-hid/flash.sh
+++ pocket-reform-keyboard-fw/pocket-hid/flash.sh
@@ -1,6 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 
-sudo picotool load build/pocket-hid.uf2 --bus 1 -f
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+	echo "you need to run this as root (for example by using sudo)" >&2
+	exit 1
+fi
+
+if [ ! -e build/pocket-hid.uf2 ]; then
+	echo "build/pocket-hid.uf2 does not exist -- did you forget to compile?" >&2
+	exit 1
+fi
+
+echo "To put your keyboard into programming mode, open the OLED menu by" >&2
+echo "pressing and holding the hyper key, followed by the enter key." >&2
+echo "Once you are in the oled menu, press X to enter programming mode." >&2
+echo "Once you press Enter, you have 10 seconds to open the OLED menu" >&2
+echo "and press X." >&2
+echo "Press the Enter key once you are ready" >&2
+# shellcheck disable=SC2034
+read -r enter
+sleep 10
+
+picotool load build/pocket-hid.uf2 --bus 1 -f
 sleep 1
-sudo picotool reboot --bus 1 -f
-
+picotool reboot --bus 1 -f

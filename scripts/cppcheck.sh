cfba284c89121ca99bbc54f4fec53d3e34f0ee1f
Lukas F. Hartmann <lukas@mntre.com>
2025-09-20 16:03:45 +0200

    cppcheck: actually fail CI on warnings/errors, surface more problems for keyboard fw

--- scripts/cppcheck.sh
+++ scripts/cppcheck.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -ex
+
 # keyboard
-cppcheck --error-exitcode=1 --check-level=exhaustive -D USBD_DESC_STR_MAX=40 -DHID_REPORT_ID= pocket-reform-keyboard-fw/pocket-hid/src
+cppcheck --suppress=constParameterPointer --enable=warning --enable=style --error-exitcode=1 --check-level=exhaustive -D USBD_DESC_STR_MAX=40 -DHID_REPORT_ID= pocket-reform-keyboard-fw/pocket-hid/src
 
 # system controller
-cppcheck --error-exitcode=1 --check-level=exhaustive -D USBD_DESC_STR_MAX=40 pocket-reform-sysctl-fw/src
+cppcheck --enable=warning --error-exitcode=1 --check-level=exhaustive -D USBD_DESC_STR_MAX=40 pocket-reform-sysctl-fw/src

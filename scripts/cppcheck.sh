23680193bbcd68d4a5abb06b641e0d5c52aa7afa
Lukas F. Hartmann <lukas@mntre.com>
2025-09-20 15:52:33 +0200

    cppcheck: exit error code for CI

--- scripts/cppcheck.sh
+++ scripts/cppcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # keyboard
-cppcheck --check-level=exhaustive -D USBD_DESC_STR_MAX=40 -DHID_REPORT_ID= pocket-reform-keyboard-fw/pocket-hid/src
+cppcheck --error-exitcode=1 --check-level=exhaustive -D USBD_DESC_STR_MAX=40 -DHID_REPORT_ID= pocket-reform-keyboard-fw/pocket-hid/src
 
 # system controller
-cppcheck --check-level=exhaustive -D USBD_DESC_STR_MAX=40 pocket-reform-sysctl-fw/src
+cppcheck --error-exitcode=1 --check-level=exhaustive -D USBD_DESC_STR_MAX=40 pocket-reform-sysctl-fw/src

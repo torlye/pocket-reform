0e1c7b41b92b73bbcb65a57c2e323103abebec1c
Chris Hofstaedtler <chris@hofstaedtler.name>
2025-09-27 16:52:13 +0200

    Upload firmware builds to LVFS
    Skip the upload job on unprotected branches, and when the protected var
    is unavailable, like on forks.

---
+++ upload-fw.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [ -z "${1:-}" ] || [ -z "${2:-}" ] || [ -z "${3:-}" ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    echo "Usage: upload-fw.sh target path/to/firmware.cab path/to/lvfs-creds/netrc"
+    exit 2
+fi
+
+set -uo pipefail
+
+RESULT=$(curl -f \
+    --netrc-file "$3" \
+    -X POST \
+    -F target="$1" \
+    -F file=@"$2" \
+    https://fwupd.org/lvfs/upload/token)
+RC=$?
+if [ $RC != 0 ]; then
+    echo "Uploading firmware failed with exit code $RC"
+    exit $RC
+fi
+
+if ! jq -e '.success == true' <<< "$RESULT" ; then
+    echo "Uploading firmware failed, remote said:"
+    echo "$RESULT"
+    exit 1
+else
+    echo "$RESULT"
+fi

4f9121cad28807912dcad413040e2a34596fc6d9
Chris Hofstaedtler <chris@hofstaedtler.name>
2025-09-28 17:54:42 +0200

    cppcheck: check pocket-reform-shared-fw too

--- scripts/cppcheck.sh
+++ scripts/cppcheck.sh
@@ -3,8 +3,8 @@
 set -ex
 
 # keyboard
-cppcheck --suppress=subtractPointers --suppress=constParameterPointer --enable=warning --enable=style --error-exitcode=1 --check-level=exhaustive -D USBD_DESC_STR_MAX=40 -DHID_REPORT_ID= pocket-reform-keyboard-fw/pocket-hid/src
+cppcheck --suppress=subtractPointers --suppress=constParameterPointer --enable=warning --enable=style --error-exitcode=1 --check-level=exhaustive -D USBD_DESC_STR_MAX=40 -DHID_REPORT_ID= pocket-reform-keyboard-fw/pocket-hid/src pocket-reform-shared-fw/src
 
 # system controller
 # TODO: fix 32 bit == unsigned long issue
-cppcheck --suppress=badBitmaskCheck --suppress=constParameterPointer --suppress=invalidPrintfArgType_uint --enable=warning --enable=style --error-exitcode=1 --check-level=exhaustive -D USBD_DESC_STR_MAX=40 pocket-reform-sysctl-fw/src
+cppcheck --suppress=badBitmaskCheck --suppress=constParameterPointer --suppress=invalidPrintfArgType_uint --enable=warning --enable=style --error-exitcode=1 --check-level=exhaustive -D USBD_DESC_STR_MAX=40 pocket-reform-sysctl-fw/src pocket-reform-shared-fw/src
